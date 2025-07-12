4ab1f7948c5702feec529866ce84c88f3adc95a3
njayantiq <nikhil.jayant@iqsetters.com>
2025-07-12 20:32:43 +0530

    feat: add user authentication context with login, signup, and logout functionality

---
+++ src/context/userAuthContext.tsx
@@ -0,0 +1,87 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+import { auth } from "@/firebaseConfig";
+
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  type User,
+} from "firebase/auth";
+
+interface IUserAuthProviderProps {
+  children: React.ReactNode;
+}
+
+type AuthContextData = {
+  user: User | null;
+  logIn: typeof logIn;
+  signUp: typeof signUp;
+  logOut: typeof logOut;
+  googleSignIn: typeof googleSignIn;
+};
+
+const logIn = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const signUp = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const logOut = () => {
+  signOut(auth);
+};
+
+const googleSignIn = () => {
+  const googleAuthProvider = new GoogleAuthProvider();
+  return signInWithPopup(auth, googleAuthProvider);
+};
+
+// Below are initial values for the context 👇
+export const userAuthContext = createContext<AuthContextData>({
+  user: null,
+  logIn,
+  signUp,
+  logOut,
+  googleSignIn,
+});
+
+export const userAuthProvider: React.FunctionComponent<
+  IUserAuthProviderProps
+> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+
+      return () => {
+        unsubscribe();
+      };
+    });
+  });
+
+  const value: AuthContextData = {
+    user,
+    logIn,
+    signUp,
+    logOut,
+    googleSignIn,
+  };
+
+  return (
+    <userAuthContext.Provider value={value}>
+      {children}
+    </userAuthContext.Provider>
+  );
+};
+
+export const useUserAuth = () => {
+  return useContext(userAuthContext);
+};
