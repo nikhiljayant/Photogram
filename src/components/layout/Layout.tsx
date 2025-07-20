9bfee455f8140cb33d9a2a3eb87fdb4bb25bbae4
njayantiq <nikhil.jayant@iqsetters.com>
2025-07-20 16:46:53 +0530

    feat: enhance Post component with form inputs, labels, and submit button; add Textarea component for better user experience

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,12 +7,13 @@ interface ILayoutProps {
 
 const Layout: React.FunctionComponent<ILayoutProps> = ({ children }) => {
   return (
-    <div className="flex bg-white">
+    <div className="flex bg-white w-screen">
       {/* Sidebar */}
       <aside className="flex gap-x-4 bg-gray-800 fixed top-0 left-0 z-40 lg:w-60 h-screen">
         <Sidebar />
       </aside>
-      <div className="lg:ml-60 lg:mr-60 p-8 flex-1 ml-36">{children}</div>
+      <div className="lg:ml-60 lg:mr-60 p-8 flex-1 ml-36 h-screen w-full">{children}</div>
+      {/* User List */}
       <aside className="hidden lg:block bg-gray-800 fixed top-0 right-0 z-40 lg:w-60 h-screen">
         <UserList />
       </aside>
--- src/components/pages/post/Post.tsx
+++ src/components/pages/post/Post.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const Post = () => {
+  // 1:38:17
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex justify-center">
+      <div className="border max-w-3xl w-full">
+        <h3 className="bg-slate-800 text-white text-center text-lg p-2">
+          Create Post
+        </h3>
+        <div className="p-8">
+          <form>
+            <div className="flex flex-col">
+              <Label className="mb-4" htmlFor="photoCaption">
+                Photo Caption
+              </Label>
+              <Textarea
+                className="mb-8"
+                id="photoCaption"
+                placeholder="What's in your photo?"
+              />
+            </div>
 
-export default Post
+            <div className="flex flex-col">
+              <Label className="mb-4" htmlFor="photoCaption">
+                Photo
+              </Label>
+            </div>
+            <Button type="submit" className="mt-8 w-32">
+              Post
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
