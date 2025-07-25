36cd714662d69b610de3c8b04eb785a40350f80e
nikhiljayant <njayant86@gmail.com>
2025-07-25 23:39:40 +0530

    feat: enhance FileUploader component with additional imports and state management

--- src/components/fileUploader/FileUploader.tsx
+++ src/components/fileUploader/FileUploader.tsx
@@ -1,8 +1,12 @@
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 import { FileEntry } from "@/types/types";
 
-import { FileUploaderRegular } from "@uploadcare/react-uploader";
+import {
+  FileUploaderRegular,
+  OutputFileEntry,
+  UploadCtxProvider,
+} from "@uploadcare/react-uploader";
 import "@uploadcare/react-uploader/core.css";
 
 interface IFileUploaderProps {
@@ -14,15 +18,35 @@ const FileUploader: React.FunctionComponent<IFileUploaderProps> = ({
   fileEnter,
   onChange,
 }) => {
-  const [uploadedFiles, setUploadedFiles] = useState<OutputFileEntry<'success'>[]>([]);
+  const [uploadedFiles, setUploadedFiles] = useState<
+    OutputFileEntry<"success">[]
+  >([]);
   const ctxProviderRef = useRef<InstanceType<UploadCtxProvider>>(null);
-  
+
+  const handleRemoveClick = useCallback(
+    (uuid: OutputFileEntry['uuid']) => onChange(files.filter(f => f.uuid !== uuid)),
+    [files, onChange],
+  );
+
+  const resetUploaderState = () =>
+    ctxProviderRef.current?.uploadCollection.clearAll();
+
+  const handleChangeEvent = (files) => {
+    setUploadedFiles([
+      ...files.allEntries.filter((f) => f.status === "success"),
+    ] as OutputFileEntry<"success">[]);
+  };
+
   return (
     <div>
       <FileUploaderRegular
         sourceList="local"
         classNameUploader="uc-light"
         pubkey="49704cae29149e02a1e2"
+        multiple={true}
+        confirmUpload={false}
+        removeCopyright={true}
+        imgOnly={true}
       />
     </div>
   );
